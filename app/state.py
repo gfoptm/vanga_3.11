17b20c19382ec55f8f6af33e5795a1c6aee35bcd
gfoptm <76264357+gfoptm@users.noreply.github.com>
2025-04-28 18:27:53 +0300

    Update state.py

--- app/state.py
+++ app/state.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import torch
 from typing import Optional, Dict, Any, List
 import datetime
-
+import argparse
 from starlette.templating import Jinja2Templates
 from torch import nn
 
@@ -10,7 +10,13 @@ from torch import nn
 models: Dict[str, nn.Module] = {}
 last_forecast_times: Dict[str, int] = {}  # ключ: f"{symbol}_{interval}_{exchange}" -> forecast_time
 scheduler = BackgroundScheduler(timezone=datetime.timezone.utc)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--device", default="cuda:0", help="cpu or cuda:<idx>")
+args = parser.parse_args()
+
+device = torch.device(args.device if torch.cuda.is_available() or args.device == "cpu" else "cpu")
 
 templates = Jinja2Templates(directory="templates")
 # регистрируем фильтр для преобразования UNIX‑времени в строку
@@ -19,3 +25,4 @@ templates.env.filters["datetimeformat"] = lambda ts: (
     .fromtimestamp(ts, datetime.timezone.utc)
     .strftime("%Y-%m-%d %H:%M UTC")
 )
+
